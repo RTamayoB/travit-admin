01f160fa57a9d68caff9454ae794bcbcf587702e
AnaidOrtega <itzel.ortega9819@hotmail.com>
2024-03-12 20:23:13 -0600

    Add SASS initial setup

--- stories/atoms/Button.tsx
+++ stories/atoms/Button.tsx
@@ -1,32 +1,14 @@
-import React from 'react';
-import './button.css';
+import React from 'react'
+import './button.scss'
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  label: string
+  primary?: boolean
+  onClick?: () => void
+  backgroundColor?: string
+  size?: 'small' | 'medium' | 'large'
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
@@ -34,11 +16,15 @@ export const Button = ({
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary'
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' ',
+      )}
       {...props}
     >
       {label}
@@ -48,5 +34,5 @@ export const Button = ({
         }
       `}</style>
     </button>
-  );
-};
+  )
+}
