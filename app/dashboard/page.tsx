376cddd97835478f219ba0e35b6a443f5a8e0ca3
Rafael Personal <rtamayo.mis@outlook.com>
2024-03-19 20:15:48 -0600

    Integrated supabase

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-    return (<p>Dashboard Page</p>)
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Page() {
+    const supabase = createClient();
+    const { data: lines } = await supabase.from("lines").select();
+    return <pre>{JSON.stringify(lines, null, 2)}</pre>
 }
