faab062deb1f02bbcff382fd6b1b7e4dbacf580d
AnaidOrtega <itzel.ortega9819@hotmail.com>
2024-04-02 13:55:37 -0600

    Text Field Component

--- shared/components/atoms/TextField/TextField.tsx
+++ shared/components/atoms/TextField/TextField.tsx
@@ -1,31 +1,96 @@
-import React, { FC, ReactNode } from 'react'
+import React, {
+  ChangeEvent,
+  FC,
+  InputHTMLAttributes,
+  ReactNode,
+  useState,
+} from 'react'
 import './textField.scss'
 import { Typography } from '..'
+import { ButtonSizes } from '../../../../shared/constants'
 
-export interface TextFieldProps {
+export interface TextFieldProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  id: string
+  label?: string
+  size?: ButtonSizes
+  required?: boolean
+  helperText?: string
   leadIcon?: ReactNode
   trailIcon?: ReactNode
+  inputState?: 'default' | 'error' | 'success'
 }
 
-export const TextField: FC<TextFieldProps> = ({ leadIcon, trailIcon }) => {
+export const TextField: FC<TextFieldProps> = ({
+  id,
+  label,
+  size,
+  leadIcon,
+  disabled,
+  required,
+  onChange,
+  trailIcon,
+  inputState,
+  helperText,
+  ...props
+}) => {
+  const [isFilled, setIsFilled] = useState(false)
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIsFilled(!!e.target.value)
+    if (onChange) {
+      onChange(e)
+    }
+  }
   return (
     <div className="msv-textField">
-      <div className="msv-textField__topView">
+      <div
+        data-disabled={disabled}
+        data-filled={isFilled}
+        className={[
+          'msv-textField__topView',
+          `msv-textField__topView--${inputState === 'error' ? 'error' : 'default'}`,
+          `msv-textField__topView--${size}`,
+        ].join(' ')}
+      >
+        {label && (
+          <label htmlFor={id}>
+            <Typography bold variant="note">
+              {label}
+              {required && <span style={{ color: '#D04116' }}> *</span>}
+            </Typography>
+          </label>
+        )}
         {leadIcon}
-        <input className="" placeholder="Placeholder" />
+        <input
+          id={id}
+          name={id}
+          disabled={disabled}
+          onChange={handleChange}
+          {...props}
+        />
         {trailIcon}
       </div>
       <div className="msv-textField__helperView">
-        <span />
-        <Typography variant="note" color="#434343">
-          Helper text here
-        </Typography>
+        {inputState !== 'default' && (
+          <span className={`span span--${inputState}`} />
+        )}
+        {helperText && (
+          <Typography variant="note" color="#434343">
+            {helperText}
+          </Typography>
+        )}
       </div>
     </div>
   )
 }
 
 TextField.defaultProps = {
+  size: 'medium',
+  required: false,
   leadIcon: false,
+  disabled: false,
+  label: undefined,
   trailIcon: false,
+  inputState: 'default',
+  helperText: undefined,
 }
