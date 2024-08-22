5fb8844300c9716225a68190613b38d428f36f32
Rafael Tamayo Barajas <rtamayo.mis@outlook.com>
2024-08-22 12:50:11 -0600

    Created new files

--- app/dashboard/lines/ui/LineTable.tsx
+++ app/dashboard/lines/ui/LineTable.tsx
@@ -1,74 +1,7 @@
-import {Button, LinkButton, Typography } from '../../../../shared/components/atoms';
+import {Button, LinkButton } from '@/shared';
 import { Line } from '@/app/lib/definitions';
-import { deleteLine } from '../../../../app/dashboard/lines/[id]/edit/data/delete-line';
+import { deleteLine } from '@/app/dashboard/lines/[id]/edit/data/delete-line';
 import Table from '@/shared/components/organisms/TableView/Table/Table';
-
-/*export default function LineTableLegacy({
-  lines,
-  onFocusToggle,
-  focusedLine
-}: {
-  lines: any[],
-  onFocusToggle: (line: Line) => void,
-  focusedLine: Line | null
-}) {
-  let keys: string[] = [];
-
-  if (lines && lines.length > 0) {
-    keys = Object.keys(lines[0]).filter(key => key !== 'route_points');
-  }
-
-  const renderCellValue = (key: string, value: any) => {
-    if (key === "created_at" || key === "updated_at") {
-      return formatDateToLocal(value);
-    } else if (typeof value === 'object' && value !== null) {
-      return JSON.stringify(value);
-    } else {
-      return value;
-    }
-  };
-
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Fecha creacion</th>
-          <th>Fecha actualizacion</th>
-          <th>Numero de Linea</th>
-          <th>Numero de Linea antiguo</th>
-          <th>Unidades</th>
-          <th>ID Agencia</th>
-          <th>Tipo de transporte</th>
-          <th>Tipo de linea</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        {lines?.map((line) => (
-          <tr key={line.id}>
-            {keys.map((key) => (
-              <td key={`${line.id}-${key}`}>
-                {renderCellValue(key, line[key])}
-              </td>
-            ))}
-            <td>
-              <Button onClick={() => onFocusToggle(line)}>
-                {focusedLine && focusedLine.id === line.id ? 'Desenfocar' : 'Enfocar'}
-              </Button>
-              <LinkButton label='Editar' href={`/dashboard/lines/${line.id}/edit`}/>
-              <Button
-                onClick={() => deleteLine(line.id)}
-              >
-                  Eliminar
-              </Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}*/
 
 export default function LineTable({
     lines,
