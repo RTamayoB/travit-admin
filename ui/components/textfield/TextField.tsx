8aa235196520190631d17ace0351bbebcc3ec513
Rafael Tamayo Barajas <rtamayo.mis@gmail.com>
2024-10-24 23:51:52 -0600

    Added correct interactivity to TextField and set correct styles

--- ui/components/textfield/TextField.tsx
+++ ui/components/textfield/TextField.tsx
@@ -1,24 +1,31 @@
-import { useState } from "react";
+import { ChangeEvent, ChangeEventHandler, HtmlHTMLAttributes, InputHTMLAttributes, useState } from "react";
 import styles from "./textfield.module.scss";
 import Image from "next/image";
 
 interface TextFieldProps {
+  value: string
+  onValueChange: (value: string) => void;
   label?: string;
-  size?: "small" | "medium" | "large";
+  size?: 'small' | 'medium' | 'large'
   leadIconUrl?: string;
   errorMessage?: string
-  disabled?: false
+  disabled?: boolean
 }
 
 function TextField({
+  value,
+  onValueChange,
   label = "",
   size = "small",
   leadIconUrl,
   errorMessage,
-  disabled
+  disabled = false,
 }: TextFieldProps) {
   const [focus, setFocus] = useState(false);
-  const [value, setValue] = useState("");
+
+  function handleValueChange(e: ChangeEvent<HTMLInputElement>) {
+    onValueChange(e.target.value)
+  }
 
   function handleFocus() {
     setFocus(true);
@@ -30,28 +37,26 @@ function TextField({
     }
   }
 
-  const labelClass = `${styles[`textfield--label`]} ${
-    (focus || value) ? styles[`textfield--label--focus`] : ''
-  } ${
-    leadIconUrl && (focus || value) ? styles[`textfield--label--focus--icon`] : ''
-  } ${
-    !focus && !value && leadIconUrl ? styles[`textfield--label--icon`] : ''
-  }
-  ${
-    errorMessage ? styles[`textfield--label--message`] : ''
-  }
-  ${disabled ? styles[`textfield--label--disabled`] : ''}
+  const labelClass = `
+    ${styles["textfield--label"]} 
+    ${(focus || value) ? styles["textfield--label--focus"] : ""}
+    ${(leadIconUrl && (focus || value)) ? styles["textfield--label--focus--icon"] : ""}
+    ${(!focus && !value && leadIconUrl) ? styles["textfield--label--icon"] : ""}
+    ${errorMessage ? styles["textfield--label--message"] : ""}
+    ${disabled ? styles["textfield--label--disabled"] : ""}
   `;
 
-  const inputClass = `${styles[`textfield--input`]} ${
-    styles[`textfield--input--${size}`]} ${
-    leadIconUrl ? styles[`textfield--input--icon`] : ''
-  }
-  ${
-    errorMessage ? styles[`textfield--input--message`] : ''
-  }`;
+  const inputClass = `
+    ${styles["textfield--input"]} 
+    ${styles[`textfield--input--${size}`]} 
+    ${leadIconUrl ? styles["textfield--input--icon"] : ""}
+    ${errorMessage ? styles["textfield--input--message"] : ""}
+  `;
 
-  const messageClass =  `${styles[`textfield--message`]}  ${disabled ? styles[`textfield--message--disabled`] : ''}`;
+  const messageClass = `
+    ${styles["textfield--message"]} 
+    ${disabled ? styles["textfield--message--disabled"] : ""}
+  `;
 
   return (
     <div className={styles.textfield}>
@@ -71,7 +76,7 @@ function TextField({
         type="text"
         className={inputClass}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleValueChange}
         onFocus={handleFocus}
         onBlur={handleBlur}
         disabled={disabled}
