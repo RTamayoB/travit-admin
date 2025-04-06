afea239cf76e1321dee27d3f38e6a1c59759511d
Rafael Tamayo Barajas <rtamayo.mis@gmail.com>
2025-04-06 00:36:53 -0600

    Fix indexes

--- ui/sections/maps/index.ts
+++ ui/sections/maps/index.ts
@@ -1,4 +1,3 @@
-export { default as LineEditMap } from "./lineeditmap";
 export { default as LinesMap } from "./linesmap";
 export { default as StopEditMap } from "./stopeditmap";
 export { default as StopsMap } from "./stopsmap";
